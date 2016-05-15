03d1d992d3ea0fcc6054af67c3c8db94b65dfd32
B Chen <bchen.us@gmail.com>
2016-05-15 03:52:08 -0700

    volume estimator
    Currently, it only counts the number of voxels per intensity value.

---
+++ volume estimator/vtk_volume_estimator .cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+using namespace std;
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+
+int main(int argc, char ** argv){
+	string file_argument;
+	vector<string> split_str;
+	fstream file;
+	map<int,int> voxel_intensity_values;
+	int intensity_value = 0;
+	int file_pos = 0;
+
+	if (argc != 2){
+		cout << "You need a vtk file as an argument" << endl;
+	}
+	else{
+		file_argument = argv[1];
+		split_str = split(file_argument,'.');
+		if ( split_str[1].compare("vtk")){
+			cout << "This is not a vtk file!" << endl;
+			
+			return 0;
+		}
+	}
+	file.open(file_argument);
+	if (file.is_open()){
+
+		cout << "File is open" << endl;
+		ifstream infile{file_argument};
+		string file_contents{ istreambuf_iterator<char>(infile), istreambuf_iterator<char>() };
+		for (int i = 0;; i++){
+			if (file_contents[i] == 3){
+				file_pos = i;
+				break;
+			}
+		}
+	}
+	else{
+		cout << "File not open" << endl;
+	}
+
+	file.seekg(file_pos);
+
+	while (!file.eof()){
+		intensity_value += file.get();
+		intensity_value = intensity_value << 8;
+		intensity_value += file.get();
+		intensity_value = intensity_value << 8;
+		intensity_value += file.get();
+		voxel_intensity_values[intensity_value] += 1;
+		intensity_value = 0;
+	}
+
+	for (std::map<int, int>::iterator it = voxel_intensity_values.begin(); it != voxel_intensity_values.end(); ++it){
+		cout << "Intensity value [ " << std::hex << it->first << " ]:  " << std::dec << it->second << " voxels" <<  endl;
+	}
+
+
+	file.close();
+
+	return 0;
+}
